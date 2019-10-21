a08d14769a2a24376240d7b3c1f3d130e3c8e76d
Jean Robertou <j-rob@live.fr>
2019-10-21 21:52:07 +0200

    Add logger and connect to DB

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Routes files
-const bootcamps = require('./routes/bootcamps');
+// const logger = require('./middleware/logger');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 // Load ENV vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to DB
+connectDB();
+
+// Routes files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV == 'development') {
+	app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`));
+const server = app.listen(
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`.yellow.bold)
+);
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`.red.bold);
+	// Close server and exit process
+	server.close(() => process.exit(1));
+});
