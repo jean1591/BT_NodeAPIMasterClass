5823016a15c153cea25c930fdc2bd510b62f20a0
Jean Robertou <jean.robertou@whereyoulove.com>
2019-10-30 23:00:20 +0100

    Custom errors

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require("../utils/errorResponse");
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -19,11 +20,11 @@ exports.getBootcamp = async (req, res, next) => {
 	try {
 		const bootcamp = await Bootcamp.findById(req.params.id);
 		if (!bootcamp) {
-			return res.status(400).json({ success: false });
+			return next(new ErrorResponse(`Bootcamp not found with ID ${req.params.id}`, 404));
 		}
 		res.status(200).json({ success: true, data: bootcamp });
 	} catch (err) {
-		res.status(400).json({ success: false });
+		next(new ErrorResponse(`Bootcamp not found with ID ${req.params.id}`, 404));
 	}
 };
 
