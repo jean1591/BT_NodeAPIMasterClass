72f45b7e2e6620fa582bee113075f22af2dc44c9
Jean Robertou <jean.robertou@whereyoulove.com>
2019-11-13 21:39:53 +0100

    Login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,3 +21,38 @@ exports.register = asyncHandler(async (req, res, next) => {
         token
     });
 });
+
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validates e-mail and password
+    if (!email || !password) {
+        return next(new ErrorResponse("Please provide and e-mail and a password", 400));
+    }
+
+    // Check for user
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+        return next(new ErrorResponse("Invalid credentials", 401));
+    }
+
+    // Check if passwords matches
+    const isMatched = await user.matchPassword(password)
+
+    if (!isMatched) {
+        return next(new ErrorResponse("Invalid credentials", 401));
+    }
+
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({
+        success: true,
+        token
+    });
+});
