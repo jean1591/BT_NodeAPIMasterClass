4b02d635baec999697e5cb3a7f0b191623c1de08
Jean Robertou <jean.robertou@whereyoulove.com>
2019-11-21 21:33:00 +0100

    Reset password complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 // @desc    Register user
 // @route   POST /api/v1/auth/register
@@ -76,7 +77,7 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     await user.save({ validateBeforeSave: false });
 
     // Create reset url
-    const resetUrl = `${req.protocol}://${req.get("host")}/api/v1/resetpassword/${resetToken}`;
+    const resetUrl = `${req.protocol}://${req.get("host")}/api/v1/auth/resetpassword/${resetToken}`;
 
     const message = `You are receiving this email beauwe you has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
 
@@ -101,10 +102,36 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse("Email could not be sent", 500));
     }
 
-    /* res.status(200).json({
+    res.status(200).json({
         success: true,
         data: user,
-    }) */
+    })
+});
+
+
+// @desc    Reset password
+// @route   PUT /api/v1/auth/resetpassword/:resettoken
+// @access  Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+    // Get hashed token
+    const resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(req.params.resettoken)
+        .digest("hex");
+
+    const user = await User.findOne({ resetPasswordToken, resetPasswordExpire: { $gt: Date.now() }});
+
+    if (!user) {
+        return next(new ErrorResponse("Invalid token", 400));
+    }
+
+    // Set new password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
+    
+    sentTokenResponse(user, 200, res);
 });
 
 // Get token from model, create cookie and send response
