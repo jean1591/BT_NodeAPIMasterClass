66d5fe6794092d6ce610b5e549fcafe3d2b045ef
Jean Robertou <jean.robertou@whereyoulove.com>
2019-11-23 16:18:17 +0100

    Update and delete reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,8 @@ const {
     getReviews,
     getReview,
     addReview,
+    updateReview,
+    deleteReview
 } = require('../controllers/reviews');
 
 const Review = require("../models/Review");
@@ -23,6 +25,8 @@ router.route("/")
     .post(protect, authorize("user", "admin"), addReview);
 
 router.route("/:id")
-    .get(getReview);
+    .get(getReview)
+    .put(protect, authorize("user", "admin"), updateReview)
+    .delete(protect, authorize("user", "admin"), deleteReview);
 
 module.exports = router;
