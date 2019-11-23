8cb1f033953e6641f1657891bd08a4531634ab31
Jean Robertou <jean.robertou@whereyoulove.com>
2019-11-23 13:36:08 +0100

    Create user routes and controller, protect and authorize only

---
+++ routes/users.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const {
+    getUser,
+    getUsers,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../controllers/users');
+
+const User = require("../models/User");
+
+const router = express.Router({ mergeParams: true });
+
+const advancedResults = require("../middleware/advancedResults");
+const { protect, authorize } = require("../middleware/auth");
+
+// Everything below .use will use protect and authorize
+router.use(protect);
+router.use(authorize("admin"));
+
+router.route("/")
+    .get(advancedResults(User), getUsers)
+    .post(createUser);
+
+router.route("/:id")
+    .get(getUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
